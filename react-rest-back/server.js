ef7150b843248c63777c5863b1b0952c635625df
Jonathan <Jon@Alberts-MacBook-Pro.local>
2017-01-25 16:01:49 -0500

    Npm starts works now

--- react-rest-back/server.js
+++ react-rest-back/server.js
@@ -1,1 +1,9 @@
 let express = require('express');
+let app = express();
+
+
+app.listen(8000, () => {
+  console.log('Server Started on http://localhost:8000');
+  console.log('Press CTRL + C to stop server');
+});
+
--- react-rest-front/src/App.js
+++ react-rest-front/src/App.js
@@ -5,7 +5,15 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <h1>HelloWorld</h1>
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
     );
   }
 }
