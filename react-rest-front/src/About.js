4b636a82df862076558535695d87f5a0e0967844
Eric Yang <ericyang321@gmail.com>
2017-03-18 15:43:29 -0700

    Fixed bug where switching to about page removes scroll blur event listener Added an about page describing what cookbook does Added acknowledgement on the bottom right part of the screen on both tabs

--- react-rest-front/src/About.js
+++ react-rest-front/src/About.js
@@ -3,8 +3,12 @@ import React from 'react';
 const About = () => {
   return (
     <div className='content-container about'>
-      <h1 className='title'>About</h1>
-      <p>This is the about page</p>
+      <h1 className='title'>What does CookBook do?</h1>
+      <p className='description'>Ever come home to find your fridge filled with random leftover ingredients, and you have no clue what meal you can make with a bag of hot dogs, parsley, and a half stick of butter?</p>
+      <p className='description'> CookBook is here to help you find any recipes available with your current ingredients. </p>
+      <h1 className='title'>Who's the wholesome cook in the background?</h1>
+      <p className='description'>We found him on <a href=''>Death to the Stock Photo</a>. They send free high quality stock images to their subscribers every month. Check them out, they take some of the best stock photos we've ever seen. </p>
+      <div id='acknowledgements'>Recipe search powered by <a href='http://www.yummly.co/recipes'>Yummley</a></div>
     </div>
   )
 }
--- react-rest-front/src/Search.js
+++ react-rest-front/src/Search.js
@@ -5,9 +5,9 @@ class Search extends Component{
     const { fetching, value, ingredientsInput, submit_button } = this.props;
     return (
       <form className='content-container'>
-        <h1 className='title'>Cook Book</h1>
+        <h1 id='home-title'>Cook Book</h1>
         <input className='form-input' type="text" required value={value} onChange={ingredientsInput} placeholder='eg: egg pasta brocolli' />
-        <button type="submit" className="form-submit" onClick={(event) => submit_button(event)}>
+        <button type="submit" className="form-submit" onClick={(e) => submit_button(e)}>
           { !fetching && <span className='emphasis-text'>Search</span> }
           { fetching && <div className='loading'></div> }
         </button>
